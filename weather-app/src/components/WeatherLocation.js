d78ce44f1e47cc083ef0e81af5513ebb5466b6e9
Alex Ferrer <aferrerdev@gmail.com>
2020-01-21 23:20:00 +0100

    Creating first functional component using reactjs

---
+++ weather-app/src/components/WeatherLocation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * First functional component
+ * @param props 
+ */
+const WeatherLocation = (props) => (
+    <div>
+        Weather Location
+    </div>
+);
+
+export default WeatherLocation;
