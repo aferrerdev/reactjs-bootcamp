e2a220beb2623c9446e557621d363b99d93b94fe
Alex Ferrer López <aferrerdev@gmail.com>
2020-03-16 13:12:25 +0100

    Adding css to components

--- weather-app/src/components/WeatherLocation/Location.js
+++ weather-app/src/components/WeatherLocation/Location.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css'
 
 const Location = (props) => {
     const { city } = props;
     return (
-        <div><h1>{city}</h1></div>
+        <div className="locationContainer">
+            <h1>{city}</h1>
+        </div>
     );
 };
 
--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import './styles.css'
 /**
  * First functional component
  * @param props 
  */
 const WeatherLocation = (props) => (
-    <div>
+    <div className="weatherLocationContainer">
         <Location city={"Buenos Aires"} />
         <WeatherData />
     </div>
