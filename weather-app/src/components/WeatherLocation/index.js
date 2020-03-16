cedce32f183f928c8dd5663df384e05381800eb8
Alex Ferrer López <aferrerdev@gmail.com>
2020-03-16 15:20:12 +0100

    State management

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -2,14 +2,7 @@ import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css'
-import { SUN } from '../../constants/weathers';
-
-const data =  {
-    temperature: 5,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10 ms/s'
-}
+import { SUN, CLOUD } from '../../constants/weathers';
 
 /**
  * Class component: we need to use class 
@@ -17,11 +10,39 @@ const data =  {
  * @param props 
  */
 class WeatherLocation extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            city: 'Buenos Aires',
+            data: {
+                temperature: 5,
+                weatherState: SUN,
+                humidity: 10,
+                wind: '10 ms/s'
+            }
+        };
+    }
+
+    handleUpdateClick = () => {
+        this.setState({
+            city: 'Barcelona',
+            data: {
+                temperature: 20,
+                weatherState: CLOUD,
+                humidity: 80,
+                wind: '10 ms/s'
+            }
+        });
+    }
+
     render() {
+        const { city, data } = this.state;
         return (
             <div className="weatherLocationContainer">
-                <Location city={"Buenos Aires"} />
+                <Location city={city} />
                 <WeatherData data={data} />
+                <button onClick={this.handleUpdateClick}>Update</button>
             </div>
         );
     }
