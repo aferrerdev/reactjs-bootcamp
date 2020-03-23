068c9b435075836f3a4f96522af74999e7cfe19a
Alex Ferrer López <aferrerdev@gmail.com>
2020-03-23 12:35:07 +0100

    Using react-flexbox-grid

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Grid, Col, Row } from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
 
 const cities = [
@@ -20,10 +21,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <LocationList 
-          cities={cities}
-          onSelectedLocation={this.handleSelectedLocation}>
-        </LocationList>
+        <Grid>
+          <Row>
+            Titulo
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList
+                cities={cities}
+                onSelectedLocation={this.handleSelectedLocation}>
+              </LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <div className="details">
+                
+              </div>
+            </Col>
+          </Row>
+
+        </Grid>
       </div>
     )
   }
