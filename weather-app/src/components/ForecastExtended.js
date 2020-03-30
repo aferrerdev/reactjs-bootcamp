0e35ca346a970024efe5e3704341e4fe151d15d0
Alex Ferrer López <aferrerdev@gmail.com>
2020-03-30 16:37:00 +0200

    ForecastItem component

--- weather-app/src/components/ForecastExtended.js
+++ weather-app/src/components/ForecastExtended.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem'
 import './styles.css';
 
+const days = [
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes'
+];
+const data = {
+    temperature: 10,
+    humidity: 10,
+    weaherState: 'normal',
+    wind: 'normal'
+};
 class ForecastExtended extends Component {
+
+    renderForecastItemDays() {
+        return days.map(day => {
+            return <ForecastItem weekDay={day} data={data}></ForecastItem>;
+        });
+    }
+
     render() {
         const city = this.props.city;
         return (
             <div>
                 <h2 className="forecast-title">Pronóstico Extendido para <br></br>{city}</h2>
+                {this.renderForecastItemDays()}
             </div>
         )
     }
