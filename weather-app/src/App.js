69766b78d992a975999e0b5a40612ba747796fa1
Alex Ferrer López <aferrerdev@gmail.com>
2020-03-31 15:39:54 +0200

    First container using Redux

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
 import { Grid, Col, Row } from 'react-flexbox-grid';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
-import LocationList from './components/LocationList';
 import ForecastExtended from './components/ForecastExtended';
-import { setCity } from './actions';
-import PropTypes from 'prop-types';
+import LocationListContainer from './containers/LocationListContainer';
 
 const cities = [
   "Barcelona,es",
@@ -25,11 +22,6 @@ class App extends Component {
   constructor() {
     super();
     this.state = { city: null };
-  }
-
-  handleSelectedLocation = city => {
-    this.setState({ city });
-    this.props.setCity(city);
   }
 
   render() {
@@ -48,10 +40,7 @@ class App extends Component {
           </Row>
           <Row>
             <Col xs={12} md={6}>
-              <LocationList
-                cities={cities}
-                onSelectedLocation={this.handleSelectedLocation}>
-              </LocationList>
+              <LocationListContainer cities={cities}></LocationListContainer>
             </Col>
             <Col xs={12} md={6}>
               <Paper zdepth={4}>
@@ -70,12 +59,4 @@ class App extends Component {
   }
 }
 
-App.propTypes = {
-  setCity: PropTypes.func.isRequired
-};
-
-const mapDispatchToProps = dispatch => ({
-    setCity: value => dispatch(setCity(value))
-});
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
