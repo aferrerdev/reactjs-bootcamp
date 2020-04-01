5fdc3780140f9363c5069c2291ff912f61bc64fb
Alex Ferrer López <aferrerdev@gmail.com>
2020-04-01 19:17:37 +0200

    Creating Action Creators

--- weather-app/src/actions/index.js
+++ weather-app/src/actions/index.js
@@ -1,12 +1,22 @@
-import { url_base_forecast, api_key } from './../constants/api_url'
+import { url_base_forecast, api_key } from './../constants/api_url';
+import transformWeather from './../services/transformWeather';
 import transformForecast from './../services/transformForecast';
+import getUrlWeatherByCity from './../services/getUrlWeatherByCity';
 
 export const SET_CITY = 'SET_CITY';
 export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
+export const SET_WEATHER_CITY = 'SET_WEATHER_CITY';
+export const GET_WEATHER_CITY = 'GET_WEATHER_CITY';
 
 const setCity = payload => ({type: SET_CITY, payload });
 const setForecastData = payload => ({type: SET_FORECAST_DATA, payload });
+const setWeatherCity = payload => ({type: SET_WEATHER_CITY, payload });
+const getWeatherCity = payload => ({type: GET_WEATHER_CITY, payload });
 
+/**
+ * Set selected city
+ * @param {*} payload 
+ */
 export const setSelectedCity = payload => {
     return dispatch => {
         const url = `${url_base_forecast}?q=${payload}&appid=${api_key}`;
@@ -23,5 +33,25 @@ export const setSelectedCity = payload => {
                 // Modificar el estado con el resultado de la promise del (fetch)
                 dispatch(setForecastData({city: payload, forecastData }))
             });
+    };
+}
+
+/**
+ * Set Weather
+ * @param {*} payload 
+ */
+export const setWeather = payload => {
+    return dispatch => {
+        payload.forEach(city => {
+            dispatch(getWeatherCity(city));
+            const api_weather = getUrlWeatherByCity(city);
+            fetch(api_weather).then(resolve => {
+                return resolve.json();
+            }).then(weather_data => {
+                const data = transformWeather(weather_data);
+                dispatch(setWeatherCity({ city, data }))
+            });
+        });
+        
     };
 }
--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,53 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Location from './Location';
 import WeatherData from './WeatherData';
 import PropTypes from 'prop-types';
 import './styles.css'
-import transformWeather from './../../services/transformWeather';
-import getUrlWeatherByCity from './../../services/getUrlWeatherByCity';
 
-/**
- * Class component: we need to use class 
- * components when we need something to do in the lifecycle.
- * @param props 
- */
-class WeatherLocation extends Component {
+const WeatherLocation = ({ onWeatherLocationClick, city, data }) => {
+    return (
+        <div className="weatherLocationContainer" 
+             onClick={onWeatherLocationClick}>
+            <Location city={city} />
+            { data ? 
+                <WeatherData data={data} /> : 
+                <CircularProgress size={50} />}
+        </div>
+    )
+};
 
-    constructor(props) {
-        super(props);
-        const { city } = props;
-        this.state = {
-            city,
-            data: null
-        };
-    }
-
-    componentDidMount() {
-        this.updateWeather();
-    }
-
-    updateWeather = () => {
-        const api_weather = getUrlWeatherByCity(this.state.city);
-        fetch(api_weather).then(resolve => {
-            return resolve.json();
-        }).then(data => {
-            const newWeather = transformWeather(data);
-            this.setState({ data: newWeather });
-        });
-    }
-
-    render() {
-        const { onWeatherLocationClick } = this.props;
-        const { city, data } = this.state;
-        return (
-            <div className="weatherLocationContainer" onClick={onWeatherLocationClick}>
-                <Location city={city} />
-                { data ? <WeatherData data={data} /> : <CircularProgress size={50} />}
-            </div>
-        );
-    }
-}
 
 WeatherLocation.propTypes = {
     city: PropTypes.string.isRequired,
