0257c0a8876bb01afb4616f8ee1408f16d3f08eb
Alex Ferrer López <aferrerdev@gmail.com>
2020-04-04 12:31:32 +0200

    Using reduce in updateCustomer action

--- customers-app/src/redux/reducers/customers.js
+++ customers-app/src/redux/reducers/customers.js
@@ -1,5 +1,5 @@
 import { handleActions } from 'redux-actions';
-import { FECTH_CUSTOMERS, INSERT_CUSTOMER } from '../../constants';
+import { FECTH_CUSTOMERS, INSERT_CUSTOMER, UPDATE_CUSTOMER } from '../../constants';
 
 const initialState = [];
 
@@ -11,7 +11,30 @@ const handleInsertCustomer = (state, action) => {
     return [...state, action.payload];
 }
 
+const handleUpdateCustomer = (state, action) => {
+    const customerPayload = action.payload;
+    const { id } = customerPayload;
+    // [ { id: 1, name: '', ... },
+    //   { id: 2, name: '', ... },
+    //   { id: 3, name: '', ... }]
+    const customers = state;
+    const initialValue = [];
+    // .reduce(function(accumulated, customer), initialValue)
+    const newCustomers = customers.reduce((accumulated, customer) => {
+        if (customer.id == id) {
+            // Add to the accumulated result the modified customer (payload)
+            // not the one in the state
+            return [...accumulated, customerPayload];
+        }
+        else {
+            return [...accumulated, customer];
+        }
+    }, initialValue);
+    return newCustomers;
+}
+
 export const customers = handleActions({
     [FECTH_CUSTOMERS]: handleFetchCustomers,
-    [INSERT_CUSTOMER]: handleInsertCustomer
+    [INSERT_CUSTOMER]: handleInsertCustomer,
+    [UPDATE_CUSTOMER]: handleUpdateCustomer
 }, initialState);
